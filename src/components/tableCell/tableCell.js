9dc555be96d714fe49583651185a1e0084d90963
Den <DenHelsing@users.noreply.github.com>
2019-04-23 11:01:25 +0300

    added tablecell, solve linear equations func

---
+++ src/components/tableCell/tableCell.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const TableCell = ({value}) => {
+    return(
+        <div className="tableCell">
+            <span>{value}</span>
+        </div>
+    )
+}
+
+export default TableCell
