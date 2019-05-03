da68b8e1542fbcf28f1ca509d7f38a240f186ae5
Den <DenHelsing@users.noreply.github.com>
2019-05-03 22:10:05 +0300

    completely set up Xs for all the cases and made Y generation

--- src/App.js
+++ src/App.js
@@ -13,37 +13,49 @@ class App extends Component {
     xMin : [-40, -35, 15],
     xMax : [20, 15, 25],
     selectedLab : 1,
-    l : 1.73,
+    l : [1.215, 1.73],
     selectedTypeOfEq : 0,
     xNorm : [],
     xNatur : [],
     xNormArray : [],
     xNaturArray : [],
     m : 2,
-    kek : "setState Ne Srabotal"
+    yMin : 0,
+    yMax : 0,
+    yValues : []
 
+  }
+
+  start = () => {
+    return (new Promise((resolve, reject) => {
+      return (this.state.xNormArray == false && this.state.xNaturArray == false) ? resolve() : null
+    }))
   }
 
   componentDidMount(){
-    if((this.state.xNormArray == false && this.state.xNaturArray == false)) this.setUpNormValues();
-    this.setState({
-      kek : "setState srabotal"
-    }, () => console.log("kek " + this.state.kek));
+    this.start()
+                .then(() => this.setUpNormValues())
+                .then(() => this.setUpNaturValues())
+                .then(() => this.pickData())
+                .then(() => this.generateYValues())
+
+    
+    // this.setState({
+      // kek : "setState srabotal"
+    // }, () => console.log("kek " + this.state.kek));
     // console.log("kek " + this.state.kek)
   }
 
-  setUpNormValues = (ifChange="") => {
-    let {selectedTypeOfEq, l} = this.state;
-    if(ifChange) (selectedTypeOfEq = ifChange);
-    // let xNormArray = [];
-
-    let xNormLinear = [];
+  setUpNormValues = () => {
+    let {l} = this.state;
+    
+    const xNormLinear = [];
       xNormLinear[0] = [1, 1, 1, 1];
       xNormLinear[1] = [-1, -1, 1, 1];
       xNormLinear[2] = [-1, 1, -1, 1];
       xNormLinear[3] = [-1, 1, 1, -1];
 
-    let xNormInteraction = [];
+    const xNormInteraction = [];
       xNormInteraction[0] = [1, 1, 1, 1, 1, 1, 1, 1, ];
       xNormInteraction[1] = [-1, -1, -1, -1, 1, 1, 1, 1];
       xNormInteraction[2] = [-1, -1, 1, 1, -1, -1, 1, 1];
@@ -62,62 +74,58 @@ class App extends Component {
         xNormInteraction[6][index] = xNormInteraction[2][index] * xNormInteraction[3][index];
         xNormInteraction[7][index] = xNormInteraction[2][index] * xNormInteraction[3][index] * element;
       })
-
-    let xNormQuadric = [];
-      xNormQuadric = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] ,
-        [-1, -1, -1, -1, 1, 1, 1, 1, -l, l, 0, 0, 0, 0, 0], 
-        [-1, -1, 1, 1, -1, -1, 1, 1, 0, 0, -l, l, 0, 0, 0], 
-        [-1, 1, -1, 1, -1, 1, -1, 1, 0, 0, 0, 0, -l, l, 0] 
-      ];
-      for(let i = 4; i < 11; i++){
-        xNormQuadric[i] = [];
-      }
-      //  xNormQuadric[5] = []; xNormQuadric[6] = []; xNormQuadric[7] = [];
-      // xNormQuadric[8] = []; xNormQuadric[9] = []; xNormQuadric[10] = []; 
-      xNormQuadric[1].forEach((element, index) => {
-        xNormQuadric[4][index] = xNormQuadric[2][index] * element;
-        xNormQuadric[5][index] = xNormQuadric[3][index] * element;
-        xNormQuadric[6][index] = xNormQuadric[2][index] * xNormQuadric[3][index];
-        xNormQuadric[7][index] = xNormQuadric[2][index] * xNormQuadric[3][index] * element;
-      })
-      for(let i = 1; i < 4; i++){
-        xNormQuadric[i].forEach((element, index) => {
-          xNormQuadric[i + 7][index] = element*element
+    
+    let xNormQuadric = [ [], [] ];
+      for(let i = 0; i < 2; i++){
+        xNormQuadric[i] = [
+          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] ,
+          [-1, -1, -1, -1, 1, 1, 1, 1, -l[i], l[i], 0, 0, 0, 0, 0], 
+          [-1, -1, 1, 1, -1, -1, 1, 1, 0, 0, -l[i], l[i], 0, 0, 0], 
+          [-1, 1, -1, 1, -1, 1, -1, 1, 0, 0, 0, 0, -l[i], l[i], 0] 
+        ];
+        for(let j = 4; j < 11; j++){
+          xNormQuadric[i][j] = [];
+        }
+        //  xNormQuadric[5] = []; xNormQuadric[6] = []; xNormQuadric[7] = [];
+        // xNormQuadric[8] = []; xNormQuadric[9] = []; xNormQuadric[10] = []; 
+        xNormQuadric[i][1].forEach((element, index) => {
+          xNormQuadric[i][4][index] = xNormQuadric[i][2][index] * element;
+          xNormQuadric[i][5][index] = xNormQuadric[i][3][index] * element;
+          xNormQuadric[i][6][index] = xNormQuadric[i][2][index] * xNormQuadric[i][3][index];
+          xNormQuadric[i][7][index] = xNormQuadric[i][2][index] * xNormQuadric[i][3][index] * element;
         })
+        for(let j = 1; j < 4; j++){
+          xNormQuadric[i][j].forEach((element, index) => {
+            xNormQuadric[i][j + 7][index] = element*element
+          })
+        }
       }
 
+      xNormQuadric[1].forEach(element => element.pop()) 
+      
+      
+      // const xNormArray = [[xNormLinear, xNormInteraction, xNormQuadric[0]], [xNormLinear, xNormInteraction, xNormQuadric[1]]];
 
-
-
-    // if(selectedTypeOfEq === "linear"){
-    //   xNorm = xNormLinear;
-    // }else if(selectedTypeOfEq === "interaction"){
-    //   xNorm = xNormInteraction;
-    // }else if(selectedTypeOfEq === "quadric"){
-    //   xNorm = xNormQuadric;
-    // }
-    // console.log(xNorm);
-      console.log([xNormLinear, xNormInteraction, xNormQuadric])
     this.setState({
-      xNormArray : [xNormLinear, xNormInteraction, xNormQuadric]
-    }, this.setUpNaturValues([xNormLinear, xNormInteraction, xNormQuadric], selectedTypeOfEq))
+      xNormArray : [[xNormLinear, xNormInteraction, xNormQuadric[0]], [xNormLinear, xNormInteraction, xNormQuadric[1]]]
+    }
+    , () => console.log(this.state.xNormArray)
+    // , this.setUpNaturValues([xNormLinear, xNormInteraction, xNormQuadric], selectedTypeOfEq)
+    )
     
   } 
   // , ifChange=""
 
-  setUpNaturValues = (xNormArray, ifChange="") => {
-    let {selectedTypeOfEq, xMax, xMin} = this.state;
-    if(ifChange) (selectedTypeOfEq = ifChange);
-    let xNatur = [];
+  setUpNaturValues = () => {
+    let {xMax, xMin, xNormArray} = this.state;
 
-  
     // if(selectedTypeOfEq === "linear"){  
       let xNaturLinear = [];
+
       for( let i = 1; i < 4; i++){
         xNaturLinear[i - 1] = [];
-        xNormArray[0][i].forEach((element, index) => {
-          (element === 1) ? (xNaturLinear[i - 1][index] = element*xMax[i - 1]) : (xNaturLinear[i - 1][index] = element*xMin[i - 1])
+        xNormArray[0][0][i].forEach((element, index) => {
+          (element === 1) ? (xNaturLinear[i - 1][index] = xMax[i - 1]) : (xNaturLinear[i - 1][index] = xMin[i - 1])
         })
       }
       // xNatur = xNaturLinear;
@@ -128,8 +136,8 @@ class App extends Component {
       let xNaturInteraction = [];
       for( let i = 1; i < 4; i++){
         xNaturInteraction[i - 1] = [];
-        xNormArray[1][i].forEach((element, index) => {
-          (element === 1) ? (xNaturInteraction[i - 1][index] = element*xMax[i - 1]) : (xNaturInteraction[i - 1][index] = element*xMin[i - 1])
+        xNormArray[0][1][i].forEach((element, index) => {
+          (element === 1) ? (xNaturInteraction[i - 1][index] = xMax[i - 1]) : (xNaturInteraction[i - 1][index] = xMin[i - 1])
         })
       }
       // xNaturInteraction[0] = [1, 1, 1, 1, 1, 1, 1, 1, ];
@@ -153,85 +161,107 @@ class App extends Component {
 
 
     // if(selectedTypeOfEq === "quadric"){
-      let xNaturQuadric = [];
+      let xNaturQuadric = [[], []];
       const xAverage = [];
       const delta = [];
+
+
 
       for(let i = 0; i < 3; i++){
         xAverage[i] = (xMax[i] + xMin[i])/2;
         delta[i] = xMax[i] - xAverage[i];
       }
       
-
+      for(let l = 0; l < 2; l++){
         for( let i = 1; i < 4; i++){
-          xNaturQuadric[i - 1] = []
+          xNaturQuadric[l][i - 1] = []
           for(let j = 0; j < 8; j++){
-            (xNormArray[2][i][j] === 1) ? 
-            (xNaturQuadric[i - 1][j] = xNormArray[2][i][j]*xMax[i - 1]) : (xNaturQuadric[i - 1][j] = xNormArray[2][i][j]*xMin[i - 1])
+            (xNormArray[l][2][i][j] === 1) ? 
+            (xNaturQuadric[l][i - 1][j] = xMax[i - 1]) : (xNaturQuadric[l][i - 1][j] = xMin[i - 1])
           }
         }
         for( let i = 0; i < 3; i++){
-          for( let j = 8; j < 14; j++){
-            xNaturQuadric[i][j] = xAverage[i];
+          for( let j = 8; j < ((l == 0) ? 15 : 14); j++){
+            xNaturQuadric[l][i][j] = xAverage[i];
           }
         } 
 
-
-        xNaturQuadric[0][8] = xNormArray[2][1][8]*delta[0] + xAverage[0];
-        xNaturQuadric[0][9] = xNaturQuadric[0][8];
-        xNaturQuadric[0][8] = xNormArray[2][1][8]*delta[0] + xAverage[0];
-        xNaturQuadric[0][9] = xNaturQuadric[0][8];
-        xNaturQuadric[0][8] = xNormArray[2][1][8]*delta[0] + xAverage[0];
-        xNaturQuadric[0][9] = xNaturQuadric[0][8];
-
-        xNaturQuadric[3] = []; xNaturQuadric[4] = []; xNaturQuadric[5] = []; xNaturQuadric[6] = [];
-        xNaturQuadric[7] = []; xNaturQuadric[8] = []; xNaturQuadric[9] = []; 
-        xNaturQuadric[0].forEach((element, index) => {
-          xNaturQuadric[3][index] = xNaturQuadric[1][index] * element;
-          xNaturQuadric[4][index] = xNaturQuadric[2][index] * element;
-          xNaturQuadric[5][index] = xNaturQuadric[1][index] * xNaturQuadric[2][index];
-          xNaturQuadric[6][index] = xNaturQuadric[1][index] * xNaturQuadric[2][index] * element;
-        })
         for(let i = 0; i < 3; i++){
-          xNaturQuadric[i].forEach((element, index) => {
-            xNaturQuadric[i + 7][index] = element*element
+          xNaturQuadric[l][i][i+i + 8] = xNormArray[l][2][i+1][i+i + 8]*delta[i] + xAverage[i];
+          xNaturQuadric[l][i][i+i + 9] = -xNaturQuadric[l][i][i+i + 8];
+        }
+
+        // xNaturQuadric[l][0][8] = xNormArray[l][2][1][8]*delta[0] + xAverage[0];
+        // xNaturQuadric[l][0][9] = xNaturQuadric[l][0][8];
+        // xNaturQuadric[l][1][10] = xNormArray[l][2][2][10]*delta[1] + xAverage[1];
+        // xNaturQuadric[l][1][11] = xNaturQuadric[l][1][10];
+        // xNaturQuadric[l][2][12] = xNormArray[l][2][3][12]*delta[2] + xAverage[2];
+        // xNaturQuadric[l][2][13] = xNaturQuadric[l][2][12];
+
+        for( let i = 3; i < 10; i++){
+          xNaturQuadric[l][i] = [];
+        }
+        // xNaturQuadric[l][3] = []; xNaturQuadric[l][4] = []; xNaturQuadric[l][5] = []; xNaturQuadric[l][6] = [];
+        // xNaturQuadric[l][7] = []; xNaturQuadric[l][8] = []; xNaturQuadric[l][9] = []; 
+
+        xNaturQuadric[l][0].forEach((element, index) => {
+          xNaturQuadric[l][3][index] = xNaturQuadric[l][1][index] * element;
+          xNaturQuadric[l][4][index] = xNaturQuadric[l][2][index] * element;
+          xNaturQuadric[l][5][index] = xNaturQuadric[l][1][index] * xNaturQuadric[l][2][index];
+          xNaturQuadric[l][6][index] = xNaturQuadric[l][1][index] * xNaturQuadric[l][2][index] * element;
+        })
+
+        for(let i = 0; i < 3; i++){
+          xNaturQuadric[l][i].forEach((element, index) => {
+            xNaturQuadric[l][i + 7][index] = element*element
           })
         }
+      }
+
+
+        
         // xNatur = xNaturQuadric;
       // }
-      console.log([xNaturLinear, xNaturInteraction, xNaturQuadric]);
+      // console.log([xNaturLinear, xNaturInteraction, xNaturQuadric]);
       this.setState({
-        xNaturArray : [xNaturLinear, xNaturInteraction, xNaturQuadric]
-      }, this.pickData(xNormArray, [xNaturLinear, xNaturInteraction, xNaturQuadric], selectedTypeOfEq))  
+        xNaturArray : [[xNaturLinear, xNaturInteraction, xNaturQuadric[0]], [xNaturLinear, xNaturInteraction, xNaturQuadric[1]]]
+      }
+      // , this.pickData(xNormArray, [xNaturLinear, xNaturInteraction, xNaturQuadric], selectedTypeOfEq)
+      , () => console.log(this.state.xNaturArray)
+      )  
   }
 
-  pickData = (xNormArrayNotState=[], xNaturArrayNotState=[], selectedTypeOfEqProps=null, selectedLab=null) => {
+  // pickData = (xNormArrayNotState=[], xNaturArrayNotState=[], selectedTypeOfEqProps=null, selectedLab=null) => {
+    pickData = () => {
     // const {selectedTypeOfEq} = this.state;
-    let {xNormArray, xNaturArray} = this.state;
+    let {xNormArray, xNaturArray, selectedTypeOfEq, selectedLab} = this.state;
 
-    let selectedTypeOfEq;
+    // let selectedTypeOfEq;
 
-    if (selectedTypeOfEqProps === null) {
-       const {selectedTypeOfEqState} = this.state;
-       selectedTypeOfEq = selectedTypeOfEqState;
-    } else {
-      selectedTypeOfEq = selectedTypeOfEqProps;
-    }
-    if(xNormArray == false && xNaturArray == false) {
-      (xNormArray = xNormArrayNotState);
-      (xNaturArray = xNaturArrayNotState);
-    }
+    // if (selectedTypeOfEqProps === null) {
+    //    const {selectedTypeOfEqState} = this.state;
+    //    selectedTypeOfEq = selectedTypeOfEqState;
+    // } else {
+    //   selectedTypeOfEq = selectedTypeOfEqProps;
+    // }
+    // if(xNormArray == false && xNaturArray == false) {
+    //   (xNormArray = xNormArrayNotState);
+    //   (xNaturArray = xNaturArrayNotState);
+    // }
 
-    const xNorm = xNormArray[selectedTypeOfEq],
-          xNatur = xNaturArray[selectedTypeOfEq];
+    const xNorm = xNormArray[selectedLab][selectedTypeOfEq],
+          xNatur = xNaturArray[selectedLab][selectedTypeOfEq];
     
-          console.log(xNormArrayNotState);
-          console.log(xNaturArrayNotState);
-    console.log(xNormArray);
-    console.log(xNaturArray);
-    console.log(selectedTypeOfEq);
+          // console.log(xNormArrayNotState);
+          // console.log(xNaturArrayNotState);
+    // console.log(xNormArray);
+    // console.log(xNaturArray);
+    console.log("/////////////////////////////////////////////")
+    console.log("Selected type = " + selectedTypeOfEq);
+    console.log("Selected lab = " + selectedLab);
     console.log(xNorm);
     console.log(xNatur);
+    console.log("/////////////////////////////////////////////")
     
     this.setState({
       xNorm,
@@ -240,23 +270,97 @@ class App extends Component {
 
   }
 
+  generateYValues = () => {
+    const {selectedLab, selectedTypeOfEq, m, xMin, xMax, xNatur} = this.state;
+    const yValues = [];
+
+    if (!selectedLab) {
+      const yMin = 200 + xMin.reduce( (prev, curr) => prev + curr ) / 3;
+      const yMax = 200 + xMax.reduce( (prev, curr) => prev + curr ) / 3;
+      
+      
+      for(let i = 0; i < m; i++){
+        yValues[i] = [];
+        for (let j = 0; j < 15; j++){
+          yValues[i][j] = this.randomInteger(yMin, yMax)
+        }
+      }
+      
+      this.setState({
+        yValues,
+        yMin,
+        yMax
+      }, () => console.log(this.state.yValues))
+    }
+
+    //    DLya labi 6
+    for(let i = 0; i < m; i++){
+      yValues[i] = [];
+      if(selectedTypeOfEq == 2){
+        for (let j = 0; j < 14; j++){
+          yValues[i][j] = 2.2 + 1.6*xNatur[0][i] + 9.2*xNatur[1][i] + 9.5*xNatur[2][i] + 0.8*xNatur[7][i] +0.7*xNatur[8][i] + 6.5*xNatur[9][i] + 0.2*xNatur[3][i] + 0.9*xNatur[4][i] + 8.7*xNatur[5][i] + 9.1*xNatur[6][i] + Math.random()*10 - 5;
+        }
+      }
+      if(selectedTypeOfEq == 1){
+        for (let j = 0; j < 8; j++){
+          yValues[i][j] = 2.2 + 1.6*xNatur[0][i] + 9.2*xNatur[1][i] + 9.5*xNatur[2][i] + 0.2*xNatur[3][i] + 0.9*xNatur[4][i] + 8.7*xNatur[5][i] + 9.1*xNatur[6][i] + Math.random()*10 - 5;
+        }
+      }
+      if(selectedTypeOfEq == 0){
+        for (let j = 0; j < 4; j++){
+          yValues[i][j] = 2.2 + 1.6*xNatur[0][i] + 9.2*xNatur[1][i] + 9.5*xNatur[2][i] + Math.random()*10 - 5;
+        }
+      }
+    }
+    this.setState({
+      yValues
+    }, () => console.log(this.state.yValues))
+
+  }
+
+  addAColumn = () => {
+    const {m} = this.state;
+    const newColumn = [];
+    for (let j = 0; j < 8; j++){
+      newColumn[j] = this.randomInteger(+this.state.yMin, +this.state.yMax)
+    }
+    const yValues1 = [...this.state.yValues, newColumn];
+    const newM = m + 1;
+
+    return(
+      this.setState({
+        yValues : yValues1,
+        m : newM
+      })
+    )
+  }
+
+  randomInteger = (min, max) => {
+    let rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
+
+
   handleTypeChange = (event) => {
     // console.log(event)
     this.setState({
       selectedTypeOfEq : event.target.value
       }
       // , console.log(this.state.selectedTypeOfEq)
-      , this.pickData(undefined, undefined, event.target.value)
+      // , this.pickData(undefined, undefined, event.target.value)
+      , () => this.pickData()
     )
   }
 
   handleLabChange = (event) => {
-    const l = (event.target.value == 1) ? 1.73 : 1.215
+    
     // console.log(event.target.value + " " + this.state.selectedLab)
     this.setState({
       selectedLab : event.target.value,
-      l : l
+      m : ( (event.target.value == 0) ? 3 : 2 )
       }
+      , () => this.pickData()
       // , console.log(this.state.selectedTypeOfEq)
       // , 
       // this.setUpNormValues(event.target.value)
